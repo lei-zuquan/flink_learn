54be069209f0b2db0b751ba817b73bacec0f2e6f
leizuquan1990 <843291011@qq.com>
2020-04-17 09:12:27 +0800

    # 新增flink操作kafka示例

---
+++ src/main/scala/com/scala/core/C_03StreamApiApp.scala
@@ -0,0 +1,50 @@
+package com.scala.core
+
+import com.alibaba.fastjson.JSON
+import com.scala.core.bean.Startuplog
+import com.scala.core.util.MyKafkaUtil
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.streaming.api.scala.{DataStream, KeyedStream, SplitStream, StreamExecutionEnvironment}
+import org.apache.flink.api.scala._
+
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 7:17 上午 2020/4/17
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+object C_03StreamApiApp {
+  def main(args: Array[String]): Unit = {
+    val environment: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // 设置并行度
+    environment.setParallelism(2)
+
+    val kafkaConsumer = MyKafkaUtil.getConsumer("GMALL_STARTUP")
+
+    val dstream: DataStream[String] = environment.addSource(kafkaConsumer)
+
+
+    // DataStream这种流是无状态
+    //val startuplogDstream: DataStream[Startuplog] = dstream.map { jsonStr => JSON.parseObject(jsonStr, classOf[Startuplog]) }
+    dstream.print()
+
+//    /**
+//     * 统计各个渠道的累计个数
+//     */
+//    // KeyedStream这种流是有状态的
+//    val chKeyedStream: KeyedStream[(String, Int), Tuple] = startuplogDstream.map(startuplog => (startuplog.ch, 1)).keyBy(0)
+//
+//    val chSumDstream: DataStream[(String, Int)] = chKeyedStream.reduce((ch1, ch2) => (ch1._1, ch1._2 + ch2._2))
+//    //dstream.print()
+//    chSumDstream.print("开头：").setParallelism(1)
+
+//G
+
+    environment.execute()
+  }
+
+}
---
+++ src/main/scala/com/scala/core/util/MyKafkaUtil.scala
@@ -0,0 +1,43 @@
+package com.scala.core.util
+
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 7:18 上午 2020/4/17
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+/*
+<!-- https://mvnrepository.com/artifact/org.apache.flink/flink-connector-kafka-0.11 -->
+<dependency>
+<groupId>org.apache.flink</groupId>
+<artifactId>flink-connector-kafka-0.11_2.11</artifactId>
+<version>1.7.0</version>
+</dependency>
+*/
+object MyKafkaUtil {
+  val prop = new Properties()
+
+  val kafka_broker_list = "node-01:9092,node-02:9092,node-03:9092"
+  prop.setProperty("bootstrap.servers", kafka_broker_list)
+  prop.setProperty("group.id", "gmall")
+
+  def getConsumer(topic:String ):FlinkKafkaConsumer011[String]= {
+    val myKafkaConsumer:FlinkKafkaConsumer011[String] =
+      new FlinkKafkaConsumer011[String](topic, new SimpleStringSchema(), prop)
+
+    myKafkaConsumer
+
+
+  }
+
+
+
+}
